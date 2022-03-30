a6711eacc5dea822876e430a52e896fb09398997
yanisse-saddek <yanisse321@gmail.com>
2022-03-30 10:20:02 +0200

    update

---
+++ index.js
@@ -0,0 +1,148 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server)
+
+var userArrayList = []
+app.get('/', (req, res)=>{
+    res.sendFile(`${__dirname}/index.html`)
+})
+app.get('/*', (req, res)=>{
+    res.sendFile(`${__dirname}/${req.path}`)
+})
+
+var tokenList = ["token"]
+var images = [
+    "https://theawesomedaily.com/wp-content/uploads/2018/03/cats-wearing-glassess-29-1.jpg",
+    "https://theawesomedaily.com/wp-content/uploads/2018/03/cats-wearing-glassess-28-1.jpeg",
+    "https://theawesomedaily.com/wp-content/uploads/2018/03/cats-wearing-glassess-27-1.jpeg",
+    "https://theawesomedaily.com/wp-content/uploads/2018/03/cats-wearing-glassess-26-1.jpeg",
+    "https://theawesomedaily.com/wp-content/uploads/2018/03/cats-wearing-glassess-25-1.jpeg",
+];
+
+io.on('connection', (socket)=>{
+    socket.on('get-token', ()=>{
+        tokenList.push(socket.id)
+        socket.emit('get-token', socket.id)
+    })
+
+    socket.on('dataPseudo', dataPseudo=>{
+        var isNew = true
+        userArrayList.map(user=>{
+            if(user.id == dataPseudo.id){
+                isNew = false
+            }
+        })        
+        if(tokenList.includes(dataPseudo.id) && isNew && dataPseudo.pseudo.length > 4){
+            var rand = Math.floor(Math.random() * images.length)
+            var randImg = images[rand]
+            var newUser = {
+                id:socket.id, 
+                pseudo:escapeHtml(dataPseudo.pseudo),
+                image:randImg,
+            }
+            var Bot = {
+                pseudo:"Bot", 
+                image:"https://theawesomedaily.com/wp-content/uploads/2018/03/cats-wearing-glassess-25-1.jpeg",
+                date:getDate()
+            }
+            userArrayList.push(newUser)
+            socket.emit('join', {newUser, Bot})
+            socket.broadcast.emit('newUserJoin', {newUser, Bot})
+            io.emit('user-count', userArrayList.length)
+            console.log(userArrayList)
+        }
+    })
+    socket.on('newMessage', dataMessage=>{
+        if(tokenList.includes(dataMessage.id) && dataMessage.message || dataMessage.files){
+            var messageInfo = {
+                message:escapeHtml(dataMessage.message),
+                date:getDate(),
+                user:getUser(dataMessage.id),
+                image:getImage(dataMessage.id),
+                room:escapeHtml(dataMessage.room),
+                files:dataMessage.files
+            }
+            io.emit('newUserMessage', {messageInfo})
+            socket.broadcast.emit('room-msg', dataMessage.room)
+        }
+    })
+    
+    socket.on('is-active', dataActive=>{
+        if(tokenList.includes(dataActive.id)){
+            userArrayList.map((user, index)=>{
+                if(user.id == dataActive.id){
+                    userArrayList[index].actif = dataActive.actif
+                }
+            })
+            io.emit('is-active', userArrayList)
+        }
+    })
+
+    socket.on('disconnect', () =>{
+        userArrayList.map((user, index)=>{
+           if(user.id == socket.id){
+            userArrayList.splice(index, 1)
+           }
+        })
+        // countConnected--
+        io.emit('user-count', userArrayList.length)
+        // io.emit('leave-message', socket.leaveMsg)
+    })
+    socket.on('new-mp', (dataNewMp)=>{
+        if(tokenList.includes(dataNewMp.id)){
+            userArrayList.map(user=>{
+            if(user.id == dataNewMp.id){
+                io.to(dataNewMp.data.data.id).emit('new-mp', {dataNewMp, user})
+            }
+        })
+        }
+    })
+
+})
+
+server.listen(3000, ()=>{
+    console.log('serveur demarré sur le port 3000')
+})
+
+function escapeHtml(text) {
+    var map = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': "&quot;",
+        "'": "&#039;",
+    };
+
+    return text.replace(/[&<>"']/g, function (m) {
+        return map[m];
+    });
+}
+function getDate(){
+    var date = new Date();
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    var heureMinute = hour.toString() + ":" + minutes.toString();
+    return heureMinute
+}
+function getUser(id){
+    var userPseudo = null
+    userArrayList.map(user=>{
+        if(user.id == id){
+            console.log('-------------')
+            console.log("oui c bueno la", user.pseudo)
+            userPseudo =  user.pseudo
+        }
+    })
+    return userPseudo
+}
+function getImage(id){
+    var userImage = null
+    userArrayList.map(user=>{
+        if(user.id == id){
+            console.log('-------------')
+            console.log("oui c bueno la", user)
+            userImage=  user.image
+        }
+    })
+    return userImage
+}
---
+++ script.js
@@ -0,0 +1,333 @@
+    var socket = io();
+    var token = null
+    var imageId = []
+    var filesList = []
+    var room = "global"
+    var lastMsg = ""
+    var lastMsgPv = null
+    socket.emit('get-token')
+    socket.on('get-token', newToken=>{token=newToken})
+
+    $('#logBtn').click((e)=>{
+        console.log('ok?')
+        e.preventDefault()
+        socket.emit('dataPseudo', {pseudo: $('#pseudo').val(), id:token})
+        $('#pseudo').val('')
+    })
+
+    socket.on('join', data=>{
+        $('.login').css('display', 'none')
+        $(".msg-list>.global").append(`
+            <div class="message ${data.Bot.pseudo} actual">
+                <div class="user">
+                    <img  height="50px" src=${data.Bot.image} />
+                    <div class="name">${data.Bot.pseudo}</div>
+                    <p>${data.Bot.date}</p>
+                </div>
+                <div class="text">
+                <p>Salut, ${data.newUser.pseudo} !</p>
+                </div>
+            </div>
+        `);
+    })
+    socket.on('newUserJoin', data=>{
+        console.log('un utilisateur a join')
+        $(".msg-list>.global").append(`
+        <div class="message ${data.Bot.pseudo} actual">
+            <div class="user">
+                <img  height="50px" src=${data.Bot.image} />
+                <div class="name">${data.Bot.pseudo}</div>
+                <p>${data.Bot.date}</p>
+            </div>
+            <div class="text">
+            <p>${data.newUser.pseudo} nous à rejoinds !</p>
+            </div>
+        </div>
+    `);
+    })
+
+    $('#btn').click(()=>{
+        socket.emit('newMessage', {room:room,message:$('#message').val(),files:filesList, id:token})
+        $("#message").val("");
+        $('.file-preview').empty()
+        filesList = []
+        imageid = []
+    })
+
+    socket.on('newUserMessage', data=>{
+        console.log('nvo msg', data)
+        files = data.messageInfo.files
+        message = data.messageInfo.message.split(' ');
+        var messageHTML = ""
+        var imageHTML = ""
+        message.map((word) => {
+            var checkLink = word.split('')
+            var isLink = false
+            var isImage = false 
+            var wordLowerCase = word.toLowerCase()
+            console.log(checkLink)
+            if(word.substring(word.length - 4) == ".jpg" || word.substring(word.length - 4) == ".png" ){
+                isImage = true
+            }
+            else if(checkLink.includes('.')){
+                isLink = true
+            }
+            if ((wordLowerCase.startsWith("http://") || wordLowerCase.startsWith("https://"))&& isLink){
+                messageHTML +=  "<a href=" +word +" target='_blank'>" +word +"</a>" + " "         
+            } 
+            else if((wordLowerCase.startsWith("http://") || wordLowerCase.startsWith("https://"))&& isImage){
+                imageHTML+= "<img class='messageImg' src="+word+" />"
+            }
+            else {
+                messageHTML +=   word + " "          
+            }
+        })
+    
+        if(files){
+            files.map(file=>{
+                imageHTML+= "<img class='messageImg' src="+file+" />"
+            })
+        }
+    
+        if (data.messageInfo.room == "global") {
+            if (data.messageInfo.user !== lastMsg) {
+                lastMsg = data.messageInfo.user;  
+                $(".global> .actual").removeClass("actual");  
+                $(".msg-list>.global").append(`
+                    <div class="message ${data.messageInfo.user} actual">
+                        <div class="user">
+                            <img  height="50px" src=${data.messageInfo.image} />
+                            <div class="name">${data.messageInfo.user}</div>
+                            <p>${data.messageInfo.date}</p>
+                        </div>
+                            <div class="text">
+                            <p>
+                                ${messageHTML}
+                                <div>
+                                ${imageHTML}
+                                </div>
+                            </p>
+                            </div>
+                        </div>
+                `);
+            } else {
+                $(".msg-list> .global > .actual").append(`
+                <p>
+                    ${messageHTML}
+                    <div>
+                    ${imageHTML}
+                    </div>
+                </p>
+                `);
+            }
+        }else {
+            if (data.messageInfo.user !== lastMsgPv) {
+                lastMsgPv = data.messageInfo.user;
+                $("." + data.messageInfo.room + "> .actuale").removeClass("actuale");
+                $(".msg-list> ." + data.messageInfo.room + "").append(`
+                <div class="message ${data.messageInfo.user} actuale">
+                    <div class="user">
+                        <img  height="50px" src=${data.messageInfo.image} />
+                        <div class="name">${data.messageInfo.user}</div>
+                        <p>${data.messageInfo.date}</p>
+                    </div>
+                    <div class="text">
+                    <p>
+                        ${messageHTML}
+                        <div>
+                        ${imageHTML}
+                        </div>
+                    </p>
+                    </div>
+                </div>`);
+            } else {
+                $(".msg-list> ." + data.messageInfo.room + " > .actuale").append(`
+                <p>
+                    ${messageHTML}
+                    <div>
+                    ${imageHTML}
+                    </div>
+                </p>
+                `);
+            }
+        }
+        var chatHistory = document.getElementById("msg-list");
+        chatHistory.scrollTop = chatHistory.scrollHeight;
+    })
+
+    socket.on('user-count', count=>{
+        $(".count-user").html(`
+        ${count}
+        `);
+    })
+
+    setInterval(() => {
+            if (!document.hidden) {
+                socket.emit("is-active", { pseudo: pseudo, actif: "actif", id:token});
+            } else {
+                socket.emit("is-active", { pseudo: pseudo, actif: "inactif", id:token});
+            }
+    }, 1000);
+
+    socket.on("is-active", (data) => {
+        $(".user-list").empty();
+        for (i = 0; i < data.length; i++) {
+            var infoData = {
+                data: data[i],
+                senderID:token
+            };
+            infoData = JSON.stringify(infoData);
+            if (data[i].id == token) {
+                $(".user-list").prepend(`
+                <div style="border-bottom:2px solid black"class="user-card">
+                    <div class="info">
+                        <img class="pdp" src='${data[i].image}'/>
+                        <p>${data[i].pseudo} (Moi)</p>
+                    </div>
+                </div>
+                `);
+            } else {
+                $(".user-list").append(`
+                <div class="user-card ${data[i].pseudo} ">
+                    <div class="state">
+                        <div class="${data[i].actif}"></div>
+                    </div>
+                    <div class="info">
+                        <img class="pdp" src='${data[i].image}'/>
+                        <p>${data[i].pseudo}</p>
+                    </div>
+    
+                    <div class="action-btn">
+                        <img src="https://cdn-icons-png.flaticon.com/512/773/773652.png" onclick='newMP(${infoData})'/>
+                    </div>
+                </div>
+                `);
+            }
+        }
+    });
+
+    const image_input = document.querySelector("#image_input");
+    var uploaded_image = ""
+    image_input.addEventListener('change', function() {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+    uploaded_image = reader.result;
+    var newImgId = "image" + (imageId.length + 1)
+    imageId.push(newImgId)
+    filesList.push(uploaded_image)
+    $('.file-preview').append(`
+    <div class="image-preview ${newImgId}">
+    <div class="close close-btn" onClick="removeFile('${newImgId}')">x</div>
+        <img height="80px" src="${uploaded_image}"/>
+    </div>
+    `)
+    });
+    reader.readAsDataURL(this.files[0]);
+    });
+
+    function removeFile(id){
+        filesList.splice(id-1, 1)
+        $('.'+id+'').remove()
+    }
+    
+    function newMP(data) {
+        console.log(data)
+        socket.emit("new-mp", {data:data, id:token});
+        var className = data.senderID + data.data.id;
+        var className2 = data.data.id + data.senderID;
+        var checkDiv = $(".select-room> ." + className + "").attr("class");
+        var checkDiv2 = $(".select-room> ." + className2 + "").attr("class");
+    
+        if ($(".msg-list> ." + className + "").length) {
+            $(".select-room> ." + className + "").css("display", "flex");
+        } else {
+            if (
+                $(".select-room> ." + className + "").length ||
+                $(".select-room> ." + className2 + "").length
+            ) {
+                $(".select-room> ." + className2 + "").css("display", "flex");
+            } else {
+                $(".select-room").append(`
+            <div class="${className2} room">
+                <p onclick="openMP('${className2}')">${data.data.pseudo}</p>
+                <p class="close" onClick="closeMP('${className2}')">X</p>
+            </div>
+            `);
+            }
+    
+            if (
+                $(".msg-list> ." + className + "").length ||
+                $(".msg-list> ." + className2 + "").length
+            ) {
+            } else {
+                $(".msg-list").append(`
+            <div class="${className2}"> 
+            <div/>
+            `);
+                $(".msg-list> ." + className + "").css("display", "none");
+            }
+        }
+    }
+
+    socket.on("new-mp", (data) => {
+        console.log(data)
+        var sender = data.dataNewMp.data.senderID;
+        var receiver = data.dataNewMp.data.data.id;
+        var className = sender + receiver;
+        var className2 = receiver + sender;
+        console.log('1', className, "2", className2)
+        if ($(".msg-list> ." + className + "").length) {
+            $(".select-room> ." + className + "").css("display", "flex");
+        } else {
+            if (
+                $(".select-room> ." + className + "").length ||
+                $(".select-room> ." + className2 + "").length
+            ) {
+                $(".select-room> ." + className2 + "").css("display", "flex");
+            } else {
+                $(".select-room").append(`
+            <div class="${className2} room">
+                <p onclick="openMP('${className2}')">${data.user.pseudo}</p>
+                <p class="close" onClick="closeMP('${className2}')">X</p>
+            </div>
+            `);
+            }
+            if (
+                $(".msg-list> ." + className + "").length ||
+                $(".msg-list> ." + className2 + "").length
+            ) {
+            } else {
+                $(".msg-list").append(`
+            <div class="${className2}"> 
+            <div/>
+            `);
+                $(".msg-list> ." + className2 + "").css("display", "none");
+            }
+        }
+    });
+    
+    function openMP(className) {
+        var oldRoomIDS = room;
+        room = className;
+        lastMsgPv = null;
+        $(".select-room > ." + room + " ").removeClass("new-msg");
+        $(".msg-list> .global").css("display", "none");
+        $(".msg-list> ." + oldRoomIDS + "").css("display", "none");
+        $(".msg-list> ." + className + "").css("display", "block");
+    }
+    function closeMP(className) {
+        lastMsgPv = null;
+        room = "global";
+        var checkDiv = $("." + className + "").attr("class");
+        if (checkDiv) {
+            $(".select-room> ." + className + "").css("display", "none");
+            $(".msg-list> ." + className + "").css("display", "none");
+            $(".msg-list>.global").css("display", "block");
+        }
+    }
+
+    socket.on("room-msg", (roomMsg) => {
+        if (roomMsg !== room) {
+            $(".select-room > ." + roomMsg + " ").addClass("new-msg");
+        }
+    });
